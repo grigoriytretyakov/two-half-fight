05b72b04e83a0b6d105d8ba1105810a04ef6dcf7
Grigoriy Tretyakov <gtretiykov@ptsecurity.com>
2017-09-05 23:14:57 +0300

    Пустой libgdx-проект. Пусть будет

---
+++ src/android/src/twohalffight/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package twohalffight.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import twohalffight.game.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ src/desktop/src/twohalffight/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package twohalffight.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import twohalffight.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
